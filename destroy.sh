b14d19711ea37a6a004a63c19a300f0e5f2de7be
hubajoo <joohuba@gmail.com>
2024-11-22 16:26:07 +0100

    configmap updated

--- destroy.sh
+++ destroy.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 
-echo "Destroying infrastructure..."
+if ! aws sts get-caller-identity &> /dev/null; then
+  echo "Error: AWS CLI is not logged in. Please configure your AWS credentials."
+  exit 1
+fi
 
-kubectl apply -f kubernetes/deployment.yaml 
-kubectl apply -f kubernetes/depl-service.yaml 
-kubectl apply -f kubernetes/ingress.yaml 
-kubectl apply -f kubernetes/ingress.yaml 
+echo "Initializing Kubernetes cluster..."
 
-cd terraform_cluster || terraform destroy -auto-approve
-echo "Infrastructure deleted successfully."
+set -x
+
+
+kubectl delete --validate=false -f kubernetes/deployment.yaml && \
+kubectl delete --validate=false -f kubernetes/depl-service.yaml && \
+kubectl delete --validate=false -f kubernetes/ingress.yaml && \
+kubectl delete --validate=false -f kubernetes/ingress.yaml && \
+cd terraform_cluster && \
+terraform destroy -auto-approve && \
+cd .. && \
+# Update kubeconfig
+aws eks --region eu-central-1 update-kubeconfig --name huba-eks-tf-cluster && \
+
+echo "Kubernetes cluster initialized successfully."
--- start.sh
+++ start.sh
@@ -1,15 +1,41 @@
 #!/bin/bash
 
-echo "Initializing Kubernetes cluster..."
-# Update kubeconfig
-aws eks --region eu-central-1 update-kubeconfig --name huba-eks-tf-cluster
+# Check if the AWS CLI is logged in
+if ! aws sts get-caller-identity &> /dev/null; then
+  echo "Error: AWS CLI is not logged in. Please configure your AWS credentials."
+  exit 1
+fi
 
-cd terraform_cluster  
-terraform apply -auto-approve ||
-cd .. ||
-kubectl apply -f kubernetes/deployment.yaml ||
-kubectl apply -f kubernetes/depl-service.yaml ||
-kubectl apply -f kubernetes/ingress.yaml ||
-kubectl apply -f kubernetes/ingress.yaml ||
+echo "Initializing Kubernetes cluster..."
+
+set -x
+# Change to the terraform_cluster directory and apply the Terraform configuration
+cd terraform_cluster && \
+terraform apply -auto-approve && \
+# Update kubeconfig
+aws eks --region eu-central-1 update-kubeconfig --name huba-eks-tf-cluster && \
+cd .. && \
+# Apply the Kubernetes manifests
+kubectl apply --validate=false -f kubernetes/configmap.yaml && \
+kubectl apply --validate=false  -f kubernetes/deployment.yaml && \
+kubectl apply --validate=false -f kubernetes/depl-service.yaml && \
+kubectl apply --validate=false -f kubernetes/load-balancer.yaml && \
+kubectl apply --validate=false -f kubernetes/ingress.yaml && \
+
+
+# Wait for the LoadBalancer to get an external IP
+echo "Waiting for LoadBalancer to get an external IP..."
+sleep 60  # Adjust the sleep time as needed
+
+# Output the LoadBalancer's external IP
+EXTERNAL_IP=$(kubectl get svc gameserver-deployment-loadbalancer -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
+echo "LoadBalancer External IP: $EXTERNAL_IP"
+
+# Update the ConfigMap with the external IP
+kubectl patch configmap gameserver-config -p "{\"data\":{\"LOADBALANCER_IP\":\"$EXTERNAL_IP\"}}"
+
+# Restart the deployment to pick up the new environment variable
+kubectl rollout restart deployment gameserver-deployment
+
 
 echo "Kubernetes cluster initialized successfully."
